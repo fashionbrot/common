286204f9901d38b28cc86b91b3f9c7299c75bb81
fashionbrot <fashionbrot@163.com>
2023-08-21 20:16:36 +0800

    0.0.5

--- common-util/src/main/java/com/github/fashionbrot/common/util/TreeUtil.java
+++ common-util/src/main/java/com/github/fashionbrot/common/util/TreeUtil.java
@@ -28,8 +28,9 @@ public class TreeUtil {
      * @return <code>List</code>
      */
     public static <T, R, U> List<T> buildTree(List<T> source, Function<T, R> idFunc, Function<T, R> pIdFunc, BiConsumer<T, List<T>> setChildListFunc, BiConsumer<T, U> setHasChildFunc, Predicate<T> rootPIdValue) {
-        if (ObjectUtil.isEmpty(source))
+        if (ObjectUtil.isEmpty(source)) {
             return source;
+        }
         Map<R, List<T>> map = source.stream().collect(Collectors.groupingBy(pIdFunc));
         Function<Boolean, Boolean> countFunc = f -> f;
         source.forEach(model -> {
@@ -58,13 +59,13 @@ public class TreeUtil {
      * @return List
      */
     public static <T, R, U> List<T> buildAscTree(List<T> source, Function<T, R> idFunc, Function<T, R> pIdFunc, BiConsumer<T, List<T>> setChildListFunc, BiConsumer<T, U> setHasChildFunc, Predicate<T> rootPIdValue, Comparator<T> sortValue) {
-        if (ObjectUtil.isEmpty(source))
+        if (ObjectUtil.isEmpty(source)) {
             return source;
+        }
         if (sortValue != null) {
             source = source.stream().sorted(sortValue).collect(Collectors.toList());
         }
-        List<T> tree = buildTree(source, idFunc, pIdFunc, setChildListFunc, setHasChildFunc, rootPIdValue);
-        return tree;
+        return buildTree(source, idFunc, pIdFunc, setChildListFunc, setHasChildFunc, rootPIdValue);
     }
 
 
@@ -83,19 +84,20 @@ public class TreeUtil {
      * @return List
      */
     public static <T, R, U> List<T> buildDescTree(List<T> source, Function<T, R> idFunc, Function<T, R> pIdFunc, BiConsumer<T, List<T>> setChildListFunc, BiConsumer<T, U> setHasChildFunc, Predicate<T> rootPIdValue, Comparator<T> sortValue) {
-        if (ObjectUtil.isEmpty(source))
+        if (ObjectUtil.isEmpty(source)) {
             return source;
+        }
         if (sortValue != null) {
             source = source.stream().sorted(sortValue.reversed()).collect(Collectors.toList());
         }
-        List<T> tree = buildTree(source, idFunc, pIdFunc, setChildListFunc, setHasChildFunc, rootPIdValue);
-        return tree;
+        return buildTree(source, idFunc, pIdFunc, setChildListFunc, setHasChildFunc, rootPIdValue);
     }
 
 
     public static <T> List<T> treeToList(List<T> source, Function<T, List<T>> getChildListFunc, BiConsumer<T, List<T>> setChildListFunc) {
-        if (ObjectUtil.isEmpty(source))
+        if (ObjectUtil.isEmpty(source)) {
             return source;
+        }
         List<T> target = new ArrayList<>();
         innerTraverseTreeToList(source, target, getChildListFunc);
         target.forEach(model -> setChildListFunc.accept(model, null));
@@ -116,9 +118,9 @@ public class TreeUtil {
 
     public static <T, R> void traverseTree(List<T> source, Function<T, List<T>> getChildListFunc, Function<T, R> getPropertyFunc, BiConsumer<T, R> setPropertyFunc) {
         source.forEach(model -> {
-            setPropertyFunc.accept(model, (R) getPropertyFunc.apply(model));
+            setPropertyFunc.accept(model,  getPropertyFunc.apply(model));
             List<T> childList = getChildListFunc.apply(model);
-            if (!ObjectUtil.isEmpty(childList)) {
+            if (ObjectUtil.isNotEmpty(childList)) {
                 traverseTree(childList, getChildListFunc, getPropertyFunc, setPropertyFunc);
             }
         });
@@ -126,16 +128,16 @@ public class TreeUtil {
 
 
     public static <T, R> List<T> searchTree(List<T> source, Function<T, List<T>> getChildListFunc, BiConsumer<T, List<T>> setChildListFunc, Function<T, R> getPropertyFunc, String keyWord) {
-        List<T> resultList = new ArrayList<>();
+        List<T> resultList = new ArrayList<T>();
         source.forEach(model -> {
-            R r = (R) getPropertyFunc.apply(model);
+            R r = getPropertyFunc.apply(model);
             if (r != null && r.toString().contains(keyWord)) {
                 resultList.add(model);
             } else {
                 List<T> childList = getChildListFunc.apply(model);
-                if (!ObjectUtil.isEmpty(childList)) {
+                if (ObjectUtil.isNotEmpty(childList)) {
                     List<T> subList = searchTree(childList, getChildListFunc, setChildListFunc, getPropertyFunc, keyWord);
-                    if (!ObjectUtil.isEmpty(subList)) {
+                    if (ObjectUtil.isNotEmpty(subList)) {
                         setChildListFunc.accept(model, subList);
                         resultList.add(model);
                     }
@@ -147,24 +149,20 @@ public class TreeUtil {
 
 
     public static <T, R> List<T> findChildList(List<T> source, Function<T, R> idFunc, Function<T, R> pIdFunc, Predicate<R> rootPIdValue) {
-        if (ObjectUtil.isEmpty(source))
+        if (ObjectUtil.isEmpty(source)) {
             return source;
+        }
         Map<R, List<T>> map = source.stream().collect(Collectors.groupingBy(pIdFunc));
         Map<R, List<T>> map2 = new HashMap<>();
         source.forEach(model -> {
             R r = idFunc.apply(model);
             List<T> childList = map.get(r);
-            if (!ObjectUtil.isEmpty(childList)) {
-                List<T> tList = map2.get(r);
-                if (tList == null) {
-                    tList = new ArrayList<>();
-                    map2.put(r, tList);
-                }
+            if (ObjectUtil.isNotEmpty(childList)) {
+                List<T> tList = map2.computeIfAbsent(r, k -> new ArrayList<>());
                 tList.addAll(childList);
             }
         });
-        List<T> list = map2.entrySet().stream().filter(t -> rootPIdValue.test(t.getKey())).map(Map.Entry::getValue).findAny().orElse(new ArrayList<>());
-        return list;
+        return map2.entrySet().stream().filter(t -> rootPIdValue.test(t.getKey())).map(Map.Entry::getValue).findAny().orElse(new ArrayList<>());
     }
 
 
