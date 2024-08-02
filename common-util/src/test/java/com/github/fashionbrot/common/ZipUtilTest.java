9983ba8b91127535d68721e40630748dac5531fb
fashionbrot <fashionbrot@163.com>
2024-08-02 15:54:13 +0800

    增加zip 工具类

--- common-util/src/test/java/com/github/fashionbrot/common/ZipUtilTest.java
+++ common-util/src/test/java/com/github/fashionbrot/common/ZipUtilTest.java
@@ -1,6 +1,7 @@
 package com.github.fashionbrot.common;
 
 import com.github.fashionbrot.common.util.ZipUtil;
+import com.github.fashionbrot.common.util.ZipWriterImpl;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -19,29 +20,24 @@ public class ZipUtilTest {
     }
 
     @Test
-    public void testGetFileList() {
-        // 创建一个内存中的 ZIP 文件
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        try (ZipOutputStream zos = new ZipOutputStream(baos)) {
-            addFileToZip(zos, "test1.txt", "This is a test file 1.");
-            addFileToZip(zos, "test2.md", "This is a test file 2.");
-            addFileToZip(zos, "test3.jpg", "This is a test file 3.");
-        } catch (IOException e) {
-            fail("Failed to create ZIP file in memory.");
-        }
+    public void testGetFileList() throws IOException {
+        ZipWriterImpl zipWriter=new ZipWriterImpl();
 
-        InputStream zipInputStream = new ByteArrayInputStream(baos.toByteArray());
+        zipWriter.addEntry( "test1.txt", "This is a test file 1.".getBytes());
+        zipWriter.addEntry(  "test2.md", "This is a test file 2.".getBytes());
+        zipWriter.addEntry( "test3.jpg", "This is a test file 3.".getBytes());
+
+        byte[] zipBytes = zipWriter.getZipBytes();
+
+        InputStream zipInputStream = new ByteArrayInputStream(zipBytes);
         List<ZipUtil.ZipEntity> fileList = ZipUtil.getFileList(zipInputStream, suffixSet);
 
         assertEquals("The number of files should be 2.", 2, fileList.size());
         assertTrue("test1.txt should be in the list.", fileList.stream().anyMatch(e -> e.getFileName().equals("test1.txt")));
         assertTrue("test2.md should be in the list.", fileList.stream().anyMatch(e -> e.getFileName().equals("test2.md")));
+
+        zipWriter.close();
     }
 
-    private void addFileToZip(ZipOutputStream zos, String fileName, String content) throws IOException {
-        ZipEntry entry = new ZipEntry(fileName);
-        zos.putNextEntry(entry);
-        zos.write(content.getBytes());
-        zos.closeEntry();
-    }
+
 }
