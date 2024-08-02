2d12388101f9e84e2f5a83a890cb65383a1e857d
fashionbrot <fashionbrot@163.com>
2024-08-02 14:26:28 +0800

    删除tlv序列化,tlv序列化已单独创建 com.github.fashionbrot:tlv:0.0.2

---
+++ common-util/src/test/java/com/github/fashionbrot/common/entity/RequestTest.java
@@ -0,0 +1,17 @@
+package com.github.fashionbrot.common.entity;
+
+
+
+
+public class RequestTest<T>{
+
+    private T code;
+
+    public T getCode() {
+        return code;
+    }
+
+    public void setCode(T code) {
+        this.code = code;
+    }
+}
